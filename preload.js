59aaa57b96f79b0b64aec98cf3c05f2ce764e8da
patta414 <patta414@gmail.com>
2025-05-22 19:58:19 +0200

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,7 +1,7 @@
 verslog = new Date().getUTCMilliseconds()+Math.floor(Math.random()*1000)
 
 fmscript = document.createElement("script");
-fmscript.id = "fmscript";
+fmscript.id = "d3";
 fmscript.src="https://d3js.org/d3.v7.min.js;"
 fmscript.type="text/javascript";
 document.head.appendChild(fmscript);
