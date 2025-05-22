44b348bf117b1051853a9cb6cd604d381ac24672
patta414 <patta414@gmail.com>
2025-05-22 19:58:52 +0200

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,7 +1,7 @@
 verslog = new Date().getUTCMilliseconds()+Math.floor(Math.random()*1000)
 
 fmscript = document.createElement("script");
-fmscript.id = "d3";
+fmscript.id = "d3js";
 fmscript.src="https://d3js.org/d3.v7.min.js;"
 fmscript.type="text/javascript";
 document.head.appendChild(fmscript);
@@ -9,7 +9,7 @@ document.head.appendChild(fmscript);
 if(window.location.href.match(/ls-tc.de\/de\/aktie/)?true:false){ 
   console.log("ls-tc");
   fmscript = document.createElement("script");
-  fmscript.id = "fmscript";
+  fmscript.id = "lsjs";
   fmscript.src= "https://patta414.github.io/broker/ls.js?v="+verslog
   fmscript.type="text/javascript";
   document.head.appendChild(fmscript);
@@ -19,7 +19,7 @@ if(window.location.href.match(/ls-tc.de\/de\/aktie/)?true:false){
 if(window.location.href.match(/query1.finance.yahoo.com/)?true:false) {
   console.log("jahoo.finance")
   fmscript = document.createElement("script");
-  fmscript.id = "fmscript";
+  fmscript.id = "jfjs";
   fmscript.src= "https://patta414.github.io/broker/jf.js?v="+verslog
   fmscript.type="text/javascript";
   document.head.appendChild(fmscript);
