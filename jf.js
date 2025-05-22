57a97e5061c28df515dd3f914a727ab594cbdb01
patta414 <patta414@gmail.com>
2025-05-22 20:41:42 +0200

    Update jf.js

--- jf.js
+++ jf.js
@@ -1,47 +1,86 @@
-init = function(){
-    v_svg = funAddHtmlE(document.body,"svg")
-    const svg = d3.select("svg");
-    const width = +svg.attr("width");
-    let data = JSON.parse(document.body.querySelector("pre").innerText)    
-    
-    height = +svg.attr("height");
-    margin = { top: 20, right: 30, bottom: 30, left: 50 };
-    
-        d3.json("data").then(data => {
-          const timestamps = data.chart.result[0].timestamp;
-          const closes = data.chart.result[0].indicators.quote[0].close;
-    
-          const dataset = timestamps.map((t, i) => ({
-            date: new Date(t * 1000),
-            close: closes[i]
-          }));
-    
-          const x = d3.scaleTime()
-            .domain(d3.extent(dataset, d => d.date))
-            .range([margin.left, width - margin.right]);
-    
-          const y = d3.scaleLinear()
-            .domain([d3.min(dataset, d => d.close) - 5, d3.max(dataset, d => d.close) + 5])
-            .nice()
-            .range([height - margin.bottom, margin.top]);
-    
-          const line = d3.line()
-            .x(d => x(d.date))
-            .y(d => y(d.close));
-    
-          svg.append("g")
-            .attr("transform", `translate(0,${height - margin.bottom})`)
-            .call(d3.axisBottom(x).ticks(10));
-    
-          svg.append("g")
-            .attr("transform", `translate(${margin.left},0)`)
-            .call(d3.axisLeft(y));
-    
-          svg.append("path")
-            .datum(dataset)
-            .attr("fill", "none")
-            .attr("stroke", "steelblue")
-            .attr("stroke-width", 2)
-            .attr("d", line);
-            });
+init = function(jsondata){
+    fmscript = document.createElement("style");
+fmscript.id = "pre";
+fmscript.innerHTML=`body { font-family: sans-serif; }
+    svg { background: #f4f4f4; border: 1px solid #ccc; }` 
+		document.head.appendChild(fmscript);
+
+pre = document.body.querySelector("pre")
+jsondata = JSON.parse(pre.innerText)
+pre.style.display = "none"
+v_svg = funAddHtmlE(document.body,"div","","chart",{style:'display: block;width: 800px;height: 400px;'})
+}
+
+build = function(jsondata) {
+        const timestamps = jsondata.chart.result[0].timestamp;
+    const prices = jsondata.chart.result[0].indicators.quote[0].low
+
+    // Kombiniere Zeit und Preis
+    const data = timestamps.map((ts, i) => ({
+      date: new Date(ts * 1000),
+      price: prices[i]
+    })).filter(d => d.price !== null); // filtere ungültige Werte
+
+    const margin = { top: 20, right: 30, bottom: 40, left: 60 };
+    const width = 900 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
+
+    const svg = d3.select("#chart")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    const x = d3.scaleTime()
+      .domain(d3.extent(data, d => d.date))
+      .range([0, width]);
+
+    const y = d3.scaleLinear()
+      .domain([d3.min(data, d => d.price) * 0.995, d3.max(data, d => d.price) * 1.005])
+      .range([height, 0]);
+
+    const xAxis = d3.axisBottom(x).ticks(10);
+    const yAxis = d3.axisLeft(y);
+
+    svg.append("g")
+      .attr("transform", `translate(0,${height})`)
+      .call(xAxis);
+
+    svg.append("g").call(yAxis);
+
+    svg.append("path")
+      .datum(data)
+      .attr("class", "line")
+      .attr("d", d3.line()
+        .x(d => x(d.date))
+        .y(d => y(d.price))
+      );
+
+    // Optional: Tooltip beim Hover
+    const tooltip = d3.select("body").append("div")
+      .style("position", "absolute")
+      .style("visibility", "hidden")
+      .style("background", "white")
+      //.style("border", "1px solid #ccc")
+      .style("padding", "6px")
+      .style("font-size", "12px");
+
+    svg.selectAll("circle")
+      .data(data)
+      .enter().append("circle")
+      .attr("cx", d => x(d.date))
+      .attr("cy", d => y(d.price))
+      .attr("r", 1)
+      .attr("fill", "steelblue")
+      .on("mouseover", function (event, d) {
+        tooltip.html(`Zeit: ${d.date.toLocaleTimeString()}<br>Preis: €${d.price.toFixed(2)}`)
+          .style("visibility", "visible");
+      })
+      .on("mousemove", event => {
+        tooltip
+          .style("top", (event.pageY - 10) + "px")
+          .style("left", (event.pageX + 10) + "px");
+      })
+      .on("mouseout", () => tooltip.style("visibility", "hidden"));
 }
