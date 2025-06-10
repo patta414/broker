1ccc47518cca688fb98d10098ae51d6a1e7327bc
patta414 <patta414@gmail.com>
2025-06-10 13:33:59 +0200

    Add files via upload

--- LS/resources/inject.js
+++ LS/resources/inject.js
@@ -1,1 +1,26 @@
+console.log("inject")
+async function injectRemoteCode(url) {
+	  try {
+		const response = await fetch(url);
+		const code = await response.text();
 
+		const script = document.createElement("script");
+		script.textContent = code;
+		(document.head || document.documentElement).appendChild(script);
+		script.remove();
+
+		console.log("Script erfolgreich injiziert");
+	  } catch (err) {
+		console.error("Fehler beim Laden des Scripts:", err);
+	  }
+	}
+
+
+
+window.onload = function () {
+	console.log('inject - Dokument geladen');
+	injectRemoteCode("https://patta414.github.io/broker/functionsOA.js");
+	injectRemoteCode("https://patta414.github.io/broker/preload.js");
+	
+	
+}
