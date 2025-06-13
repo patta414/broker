9427d9972752f5fa6d0442083bc68996259decd1
patta414 <patta414@gmail.com>
2025-06-13 10:14:46 +0200

    Update ls_watchlist.js

--- ls_watchlist.js
+++ ls_watchlist.js
@@ -8,11 +8,12 @@ c={
 c.initChartjs = function(){
     injectRemoteCode("https://cdn.jsdelivr.net/npm/chart.js@4.2.1/dist/chart.umd.min.js",()=>{
         injectRemoteCode("https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns@3.0.0/dist/chartjs-adapter-date-fns.bundle.min.js",()=>{
-            window.c.active=true
+            window.c.active=true;
+            c.refreshAll();
         })
     })
 }
-console.log("24") // =======================================
+console.log("25") // =======================================
 c.initChartjs()
 c.init = function(name){
     if(!(this.active)) return
